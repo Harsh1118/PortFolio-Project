b6cd3ff019ee86a7f9fd2944c2265b3f844f94fc
Harsh1118 <harshdubey1118@gmail.com>
2026-04-01 11:55:34 +0530

    Navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,7 +40,7 @@ export default function Navbar() {
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
-            AC
+            HD
           </motion.a>
 
           {/* Desktop Navigation */}
